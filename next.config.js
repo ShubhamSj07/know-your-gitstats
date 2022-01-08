bbfc3a5e4ac2fb89f0ec04475cb208fd60f2faad
ShubhamSj07 <shubhamsj077@gmail.com>
2022-01-08 16:04:37 +0530

    prettier added, nextjs configs updated!

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    domains: ["https://avatars.githubusercontent.com/"],
+  },
+};
